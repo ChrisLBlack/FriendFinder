4572d35911a1b9d1a0e38a5949e1e90da738f82d
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-01 11:11:45 -0500

    fixed survey link

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 module.exports = (app) => {
 
+    app.get("/survey", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        console.log("survey")
+    });
+
+
     app.use((req, res) => {
         res.sendFile(path.join(__dirname, "../public/home.html"));
         console.log("default")
     });
-
-    app.get("/survey"), (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-        console.log("survey")
-    };
 
 };
