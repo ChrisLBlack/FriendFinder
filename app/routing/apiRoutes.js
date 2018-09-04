b2f308c256f181195a71629112743d95fea60fff
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-04 17:37:43 -0500

    adding github links

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,10 @@ module.exports = function (app) {
   app.get("/api/friends", (req, res) => {
     return res.json(friendsData);
   });
+
+  app.post("api/friends", (req, res) => {
+    friendsData.push(req.body);
+    res.json(true);
+
+  })
 };
