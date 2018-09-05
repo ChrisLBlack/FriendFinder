bb3fafd3f7b57414e3c3d4c6a9293413fa3d021b
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-04 19:44:11 -0500

    removing a few logs

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,13 +4,10 @@ module.exports = (app) => {
 
     app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-        console.log("survey")
     });
-
 
     app.use((req, res) => {
         res.sendFile(path.join(__dirname, "../public/home.html"));
-        console.log("default")
     });
 
 };
