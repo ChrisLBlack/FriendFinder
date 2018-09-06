546e4f58702c77870e03ba7ab6c0bd8826bd08ee
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-05 22:48:35 -0500

    finishing up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 const friendsArr = [
     {
         "name": "Chris",
-        "photo": "www.google.com",
+        "photo": "https://avatars1.githubusercontent.com/u/38511950?s=400&u=9a18bd338593e9c51eaeffd85ce829cbca3bc52f&v=4",
         "scores": [
             1,
             2,
@@ -14,7 +14,38 @@ const friendsArr = [
             9,
             10
         ]
+    }, {
+        "name": "Britney",
+        "photo": "https://bossip.files.wordpress.com/2014/08/britney-spears-crazy.jpg",
+        "scores": [
+            4,
+            6,
+            7,
+            2,
+            5,
+            5,
+            7,
+            1,
+            4,
+            8
+        ]
+    }, {
+        "name": "Nick",
+        "photo": "http://www.heligods.com/attachments/nicolas-cage-crazy-dude-froot-jpg.238/",
+        "scores": [
+            1,
+            6,
+            7,
+            4,
+            7,
+            5,
+            9,
+            4,
+            8,
+            1
+        ]
     }
+
 ];
 
 module.exports = friendsArr;
