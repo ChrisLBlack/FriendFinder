068be605b3cf19beb0096beff52bf78ada79ec30
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-05 22:15:33 -0500

    can't get .post callback function to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,37 @@
 const friendsData = require('../data/friends.js');
+const path = require("path");
 
 module.exports = function (app) {
   app.get("/api/friends", (req, res) => {
     return res.json(friendsData);
   });
 
-  app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    console.log(req.body);
-    // res.json(true);
+  app.post("/api/friends", function (req, res) {
+    //pushing user input to database (friends.js)
+    const newData = req.body;
+  //   // console.log(newData.scores);
 
+    //matching friends
+    let matchName = "";
+    let matchImage = "";
+    let total = 10000;
+
+    for (let i = 0; i < friendsData.length; i++) {
+      let diff = 0;
+      
+      for (let check in newData.scores) {
+        let v = parseInt(friendsData[i].scores);
+        let s = parseInt(newData.scores[check]);
+        diff = Math.abs(v - s);
+      };
+      
+      if (diff < total){
+        total = diff;
+        matchName = friendsData[i].name;
+        matchImage = friendsData[i].photo;
+      }
+    }
+
+    friendsData.push(newData);
   });
 };
